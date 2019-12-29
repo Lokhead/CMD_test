15bb52887c84dcb82f5214a6088250f998874271
Lokhead <thor.bash@gmail.com>
2019-12-29 14:41:24 +0300

    29.12.2019 -first commit

---
+++ test_1.py
@@ -0,0 +1,16 @@
+from math import cos, sin, pi
+from cmath import sqrt
+z = 40 + 5j
+z1 = sqrt(z)
+print(z1)
+
+print(z, z1, sep="")  # вывод слитно
+print(18, 30, "00", sep=":", end="!")  # пример работы с форматом
+print(""" Test """)  # для большого объема текста с сохранением форматирования
+
+x = 12*8/5
+x1 = x2 = x3 = 5
+
+x4 = cos(5)
+print(sin(pi/2))
+# dir(math)
